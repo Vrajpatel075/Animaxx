ab9026bea4e4b68f9878b4314c0c64f1baaa3622
vraj patel <vraj75228@gmail.com>
2026-01-20 13:18:51 +0530

    added suggetion post

--- src/assets/WebPages/ViewedPost.jsx
+++ src/assets/WebPages/ViewedPost.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from 'react'
 import UniversalNav from '../htmlBlocks/UniversalNav'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import "../WebPagesCss/ViewedPost.css"
 import { FaRegHeart } from "react-icons/fa";
 import { FaShare } from "react-icons/fa";
+import "../WebPagesCss/WallpaperPg.css"
+import FooterPannel from '../htmlBlocks/FooterPannel';
 
 function ViewedPost({postdata , currMode }) {
   const [wallpaperNavOpen, setWallpaperNavOpen] = useState(false);
   const { postId } = useParams(); 
-    const [wordLimit , setWordLimit] = useState(15);
+  const [wordLimit , setWordLimit] = useState(5);
+  const navigate   = useNavigate();
+
+  // to fetch the selected post  data
   const post = postdata.find(p => p.postId === parseInt(postId));
+
+  // to fetch the recomende post accept the selected post
+  const recommendedPosts = postdata.filter(p => p.postId !== parseInt(postId));
+
+  const mainSectionsuggestion = 35;
+  const sideSectionsuggestion = 25;
+
+  function getRandomSuggestions(start,count){
+    return [...recommendedPosts]
+    .sort(() => Math.random() - 0.5)
+    .slice(start, count);
+  }
+
 
   if (!post) return <p>Post not found</p>;
 
@@ -25,24 +43,79 @@ function ViewedPost({postdata , currMode }) {
     navOpen={wallpaperNavOpen}
     setNavOpen={setWallpaperNavOpen}
     currMode={currMode}/>
+    
 
-<div className="ViewPostContainer">
-    <div className='SelectedPostContainer'>
-      <div className="ViewedImg">
-        <img src={post.imageUrl} alt={post.postOwner} />
+    <div className="ViewPostContainer">
+
+      <div className="mainSection">
+       
+        <div className='SelectedPostContainer'>
+        <div className="ViewedImg">
+          <img src={post.imageUrl} alt={post.postOwner} />
+        </div>
+          <div className="PostAuther">
+            <h2>{post.postOwner}</h2 >
+            <p className="postDescription">
+              {truncateText(post.description, wordLimit)}
+            </p>
+          </div>
+          <div className='PostActivite'>
+            <span><span className='icons'><FaRegHeart/></span>{post.likes}</span>
+            <span><span className='icons'><FaShare/></span></span>
+          </div>
       </div>
-      <div className="PostAuther">
-         <h2>{post.postOwner}</h2 >
-         <p className="postDescription">
-          {truncateText(post.description, wordLimit)}
-          </p>
+
+       <section >
+      <div className="recomendedPost">
+        {getRandomSuggestions(0,mainSectionsuggestion).map(rp=>(
+          <div 
+          className='postCard'
+          key={rp.postId}>
+            <img src={rp.imageUrl} 
+            alt={rp.postOwner} 
+            className={currMode === 'light' ? 'day' : 'night'}
+            onClick={()=> navigate(`/ViewedPost/${rp.postId}`)} 
+            />
+            <h4 className="postTitle">{rp.postOwner}</h4>
+              <p className="postDescription">
+                {truncateText(rp.description, wordLimit)}
+              </p>
+              </div>
+            ))}
+            </div>
+      </section>
+
+
       </div>
-         <div className='PostActivite'>
-          <span><span className='icons'><FaRegHeart/></span>{post.likes}</span>
-          <span><span className='icons'><FaShare/></span></span>
-         </div>
+
+      <div className="sideSection">
+       <section>
+        <div className="recomendedPost">
+          {getRandomSuggestions(0,sideSectionsuggestion).map(rp=>(
+            <div 
+          className='postCard'
+          key={rp.postId}>
+            <img src={rp.imageUrl} 
+            alt={rp.postOwner} 
+            className={currMode === 'light' ? 'day' : 'night'}
+            onClick={()=> navigate(`/ViewedPost/${rp.postId}`)} 
+            />
+            <h4 className="postTitle">{rp.postOwner}</h4>
+              <p className="postDescription">
+                {truncateText(rp.description, wordLimit)}
+              </p>
+              </div>
+          ))}
+        </div>
+       </section>
+      </div>
+    
+    
     </div>
-</div>
+
+
+
+    <FooterPannel/>
     </>
   )
 }

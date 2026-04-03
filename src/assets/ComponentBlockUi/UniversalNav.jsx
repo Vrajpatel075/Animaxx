29533f1fc7a7795e2e2031754d7dd1cf46e66900
vraj patel <vraj75228@gmail.com>
2026-04-03 23:59:01 +0530

    added dropdown for profile icon in navigation pannel

--- src/assets/ComponentBlockUi/UniversalNav.jsx
+++ src/assets/ComponentBlockUi/UniversalNav.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../ComponentBlockCss/UniversalNav.css';
 import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { TbLogout2 } from "react-icons/tb";
@@ -6,6 +6,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import SignInCheak from '../ModelBody/SignInCheak';
 import { fetchuserdata, logoutUser } from '../Redux/authSlice';
 import ExitWarring from '../ModelBody/ExitWarring';
+import { FaUser } from 'react-icons/fa';
+import { IoMdSettings } from 'react-icons/io';
 
 function UniversalNav({ navOpen, setNavOpen, showSearch }) {
   const navigate = useNavigate();
@@ -19,6 +21,23 @@ function UniversalNav({ navOpen, setNavOpen, showSearch }) {
   const { userId, profile, isLoggedIn } = useSelector((state) => state.auth);
   const currMode = useSelector((state) => state.theme.mode);
 
+  // dropdown
+  const [isDropdownOpen , setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef();
+  
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (!dropdownRef.current?.contains(e.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  return () => {
+    document.removeEventListener("mousedown", handleClickOutside);
+  };
+}, []);
+
   const navItems = [
     { label: 'GALLERY', paths: ['/Gallery', '/ViewedPost'] },
     { label: 'BLOGS', paths: ['/BlogsPg'] },
@@ -29,7 +48,7 @@ function UniversalNav({ navOpen, setNavOpen, showSearch }) {
 
   const handleProfileClick = () => {
     if (isLoggedIn) {
-      navigate(`/ProfilePg/${userId}`);
+      setShowSignInModal(false)
     } else {
       setShowSignInModal(true);
     }
@@ -41,10 +60,12 @@ function UniversalNav({ navOpen, setNavOpen, showSearch }) {
     }
   }, [userId, dispatch]);
 
+  
+
   return (
     <>
-      <div className={`wallpaper-side-nav ${navOpen ? 'show' : ''}`}>
-        <div className="mob-nav-header">
+      <div className={`side-nav ${navOpen ? 'show' : ''}`}>
+        <div className="side-nav-header">
           <button
             className="toggle-close-nav-btn"
             onClick={() => setNavOpen(false)}
@@ -125,6 +146,7 @@ function UniversalNav({ navOpen, setNavOpen, showSearch }) {
           <span> ☰ </span>
         </button>
 
+        {/* display serchbar as per needs */}
         {showSearch && (
           <div className="responsive_nav_searh">
             <input className="search_input" type="text" placeholder="Search..." />
@@ -134,11 +156,16 @@ function UniversalNav({ navOpen, setNavOpen, showSearch }) {
           </div>
         )}
 
-        <div className={`dropdown`}>
+        {/* dropdown on profile picture click */}
+        <div className="dropdown" ref={dropdownRef}>
 
-          <div className="Animaxx-logo link" tabIndex={0}>
-          <img
-            src={
+          <div 
+          className="Animaxx-logo mouseCursor" 
+          tabIndex={0}
+          onClick={() => setIsDropdownOpen(prev => !prev)}>
+
+          <img 
+          src={
               profile?.profilePicture
                 ? `http://localhost:8080/uploads/profile-pics/${profile.profilePicture}`
                 : profile?.gender === "male"
@@ -148,19 +175,65 @@ function UniversalNav({ navOpen, setNavOpen, showSearch }) {
                 : "/animax img source/animaxx_default_user_profile_picture.png"
             }
             alt="logo"
-            // onClick={handleProfileClick}
-          />
-       
-          {showSignInModal && (
-            <SignInCheak onClose={() => setShowSignInModal(false)} />
-          )}
+          onClick={handleProfileClick}/>
+
           </div>
-          <div className={`dropdown-menu ${currMode === "light" ? "light" : "dark"}`}>
-            dropdown content
+
+
+          <div className={`dropdown-menu 
+          ${ isDropdownOpen ? "active" : "" }
+          ${currMode === "light" ? "light" : "dark"}`}>
+            <div className='Profile-info'>
+              <div>
+                {/* user profile picture */}
+                <div className="profile-img">
+                  <img src={`http://localhost:8080/uploads/profile-pics/${profile.profilePicture}`} alt="" />
+                </div>
+              </div>
+              
+              {/* user first and last namewith email */}
+              <div className="profile-content">
+                <h2>{profile.firstName} {profile.lastName}</h2>
+                <p>{profile.email}</p>
+              </div>
+            </div>
+
+          <hr />
+
+          {/* Profile */}
+          <p 
+          className='dropdownlink'
+          onClick={()=> navigate(`/ProfilePg/${userId}`)}>
+            <span><FaUser/></span> 
+            <span>Profile</span>
+          </p>
+
+          {/* settings */}
+          <p 
+          className='dropdownlink'>
+            <span><IoMdSettings/></span>
+            <span>Settings</span>
+          </p>
+
+          <hr/>
+
+          {/* Logout */}
+          <p
+          onClick={() => setShowLogoutModal(true)}
+          className='dropdownlink'>
+            <span><TbLogout2/></span>
+            <span>Logout</span>
+          </p>
           </div>
 
         </div>
+      
       </div>
+
+
+         {showSignInModal && (
+            <SignInCheak onClose={() => setShowSignInModal(false)} />
+          )}
     </>
   );
 }
